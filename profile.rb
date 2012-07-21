1b912120a8feff7dc419ea0652aaf5454b34551e
Kevin Alford <zeroeth@pixelflow.org>
2012-07-20 19:58:42 -0500

    Progress bar

--- profile.rb
+++ profile.rb
@@ -1,23 +1,28 @@
-require 'ruby-prof'
+require 'rubygems'
+require 'bundler/setup'
 require File.dirname(__FILE__) + '/male_marten'
 require 'chunky_png'
+require 'progressbar'
 
 # Profile the code
 
 puts 'world'
-#world = World.new width: 1351, height: 712
-world = World.new width: 35, height: 35
+world = World.new width: 1351, height: 712
+#world = World.new width: 35, height: 35
 
 puts 'spawning'
-martens = MaleMarten.spawn_population world, 2 
+martens = MaleMarten.spawn_population world, 100
 
-RubyProf.start
+#RubyProf.start
 
-puts 'ticking'
-500.times{ print '.'; world.tick; world.to_png }
+ProgressBar.color_status
+ProgressBar.iter_rate_mode
+bar = ProgressBar.new 'ticks', 500
+500.times{ world.tick; world.to_png; bar.inc }
+bar.finish
 
-result = RubyProf.stop
+#result = RubyProf.stop
 
 # Print a flat profile to text
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(STDOUT)
+#printer = RubyProf::FlatPrinter.new(result)
+#printer.print(STDOUT)
